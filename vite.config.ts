4a0ff18cd6d9b1cb2323fe908f3987b726ea2a8d
gopherine <atharvajava@gmail.com>
2024-06-17 23:22:27 +0530

    fix: Fix onload issues with manifest.json - cache issues - remove directly embedded js in base layout"
    sometimes manifest logic was just caching js files and not updating, also there was too much bloat .. it was also observed that alpine js is not loading properly when deferred"

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), chunkSplitPlugin()],
   base: '/public/', // Ensure base path is set correctly
   build: {
     manifest: true,
     outDir: 'public',
     rollupOptions: {
-      input: resolve(__dirname, 'views/scripts/index.ts'),
+      input: {
+        main: resolve(__dirname, 'views/scripts/index.ts'),
+        moduleLoader: resolve(__dirname, 'views/scripts/module-loader.ts'),
+      },
+      output: {
+        entryFileNames: (chunk) => {
+          // Name the module-loader file without a hash
+          if (chunk.name === 'moduleLoader') {
+            return 'assets/module-loader.js';
+          }
+          // Use hashes for other entry files
+          return 'assets/[name]-[hash].js';
+        },
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      },
     },
   },
 });
